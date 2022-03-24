caf5d75e711df161c032c9c4a8cca51df6230630
oleg-medovikov <oleg.medovikov@mail.ru>
2022-03-24 15:32:02 +0300

    change dubli

--- lin/sql/zam/kolichestvo.sql
+++ lin/sql/zam/kolichestvo.sql
@@ -6,19 +6,19 @@ select kol.*, dubli.[Количество дублей] from (
             where [Тип организации] is not null
             group by [Медицинская организация],[Принадлежность],[Тип организации] ) as kol
     left join  (select itog.[Первая организация], count(*) as 'Количество дублей' from (
-                    select distinct
+					select distinct
 					fr1.УНРЗ as 'Первое УНРЗ', fr1.[Медицинская организация] as 'Первая организация'
-                     from (select distinct dbo.get_Gid(IdPatient) as 'Gid',[УНРЗ],[ФИО],[Диагноз установлен],[Медицинская организация],[СНИЛС],[Исход заболевания]
-                                    from  robo.v_FedReg) as fr1
-                            inner join (select  distinct dbo.get_Gid(IdPatient) as 'Gid',[УНРЗ],[ФИО],[Диагноз установлен],[Медицинская организация],[СНИЛС],[Исход заболевания]
-                                            from robo.v_FedReg ) as fr2
-                            on(fr1.Gid = fr2.Gid and fr1.УНРЗ <> fr2.УНРЗ and fr1.[Медицинская организация] = fr2.[Медицинская организация]
-                                    and(fr1.[СНИЛС] = 'Не идентифицирован' or fr2.[СНИЛС] = 'Не идентифицирован') 
-                                    and abs(DATEDIFF (day,fr1.[Диагноз установлен],fr2.[Диагноз установлен])) < 60
-                                    and ( (fr1.[Диагноз установлен] < fr2.[Диагноз установлен] and fr1.[Исход заболевания] != 'Выздоровление' )
-                                         or
-                                     (fr1.[Диагноз установлен] > fr2.[Диагноз установлен] and fr2.[Исход заболевания] != 'Выздоровление' )) 
-                                )
-								) as  itog 
+						from (select distinct dbo.get_Gid(IdPatient) as 'Gid',[idFedreg],[УНРЗ],[ФИО],[Диагноз установлен],[Медицинская организация],[СНИЛС],[Исход заболевания]
+									from  robo.v_FedReg) as fr1
+
+						inner join (select  distinct dbo.get_Gid(IdPatient) as 'Gid',[idFedreg],[УНРЗ],[ФИО],[Диагноз установлен],[Медицинская организация],[СНИЛС],[Исход заболевания]
+											from robo.v_FedReg ) as fr2
+							on(
+							fr1.Gid = fr2.Gid and fr1.УНРЗ <> fr2.УНРЗ 
+							and fr1.[idFedreg] - fr2.[idFedreg] < 0
+							and fr1.[Медицинская организация] = fr2.[Медицинская организация]
+							and(fr1.[СНИЛС] = 'Не идентифицирован' or fr2.[СНИЛС] = 'Не идентифицирован') 
+							and  abs(DATEDIFF (day,fr1.[Диагноз установлен],fr2.[Диагноз установлен])) < 60
+							) ) as  itog 
         group by itog.[Первая организация] ) as dubli
     on (kol.[Медицинская организация] = dubli.[Первая организация])
